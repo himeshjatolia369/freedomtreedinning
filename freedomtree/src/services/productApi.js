a8835c3e18de4b4ae1c6d6d755dca86df7616996
himeshjatolia369 <himesh.jatolia.5100781@ves.ac.in>
2026-01-07 00:41:14 +0530

    Update productApi.js

--- freedomtree/src/services/productApi.js
+++ freedomtree/src/services/productApi.js
@@ -1,11 +1,11 @@
 export const fetchProducts = async () => {
-  const res = await fetch("http://localhost:5000/api/products");
+  const res = await fetch("https://freedomtreedinning-backend.onrender.com/api/products");
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 };
 
 export const fetchProductById = async (id) => {
-  const res = await fetch(`http://localhost:5000/api/products/${id}`);
+  const res = await fetch(`https://freedomtreedinning-backend.onrender.com/api/products/${id}`);
   if (!res.ok) throw new Error("Failed to fetch product");
   return res.json();
 };
